0fdf9084aff2cb95008f58cc4df87de24f33d62c
juanraba <jrabanillog@gmail.com>
2018-07-11 10:17:10 -0400

    add ejer4

--- ejer4.rb
+++ ejer4.rb
@@ -1,4 +1,14 @@
 personas = ["Carolina", "Alejandro", "Maria Jesús", "Valentín"]
 edades = [32, 28, 41, 19]
 
-puts Hash[personas.zip edades]
+hash = Hash[personas.zip edades]
+puts hash
+
+puts '2. Crear un método que reciba el hash y
+ devuelva la edad del hash pasado como argumento'
+
+def edad(hash)
+  hash.values
+end
+
+print edad(hash)
